5d0bd2d016ddabdc958f60cd14e0b41991b51b7f
Marco Gattello <markgatte@gmail.com>
2025-05-17 22:22:39 +0100

    T-2 Add InputWrapper component with tests, storybook story, style

--- front-end/zego/.storybook/main.ts
+++ front-end/zego/.storybook/main.ts
@@ -1,22 +1,11 @@
-import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
+// .storybook/main.ts
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
-  ],
-  "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
-    "options": {}
-  },
-  "staticDirs": [
-    "../public"
-  ]
+  stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-essentials'],
+  framework: '@storybook/nextjs',
+  staticDirs: ['../public'],
 };
+
 export default config;
---
+++ front-end/zego/src/components/WrapperInput/WrapperInput.test.tsx
@@ -0,0 +1,69 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import WrapperInput, { InputType } from './WrapperInput';
+import { useState } from 'react';
+
+function TestWrapperInput() {
+    const [inputTestValue, setInputTestValue] = useState('');
+
+    return (
+        <WrapperInput
+            id="test"
+            inputPlaceHolder="Test"
+            inputName="test"
+            required={true}
+            inputValue={inputTestValue}
+            inputType={InputType['Text']}
+            onChangeFx={() => setInputTestValue}
+        />
+    );
+}
+
+function TestWrapperInputList() {
+    const [inputTestValue, setInputTestValue] = useState('');
+    const inputList = ['test 1', 'test 2', 'test 3'];
+
+    return (
+        <WrapperInput
+            id="test"
+            inputPlaceHolder="Test"
+            inputName="test"
+            required={true}
+            inputValue={inputTestValue}
+            inputType={InputType['Text']}
+            inputListId="tests"
+            inputDataTestId="list-test"
+            inputList={inputList}
+            onChangeFx={() => setInputTestValue}
+        />
+    );
+}
+
+describe('WrapperInput', () => {
+    it('renders the input', () => {
+        render(<TestWrapperInput />);
+        const input = screen.getByTestId('wrapper-input-test');
+
+        expect(input).toBeInTheDocument();
+        expect(input).toHaveAttribute('placeholder', 'Test');
+    });
+
+    it('renders the input with list', () => {
+        render(<TestWrapperInputList />);
+
+        const input = screen.getByTestId('wrapper-input-list-test');
+        const datalist = screen.getByTestId('wrapper-input-datalist-list-test');
+        const options = datalist.querySelectorAll('option');
+
+        expect(input).toBeInTheDocument();
+        expect(input).toHaveAttribute('list', 'tests');
+
+        expect(datalist).toBeInTheDocument();
+        expect(datalist).toHaveAttribute('id', 'tests');
+
+        expect(options).toHaveLength(3);
+        expect(options[0]).toHaveAttribute('value', 'test 1');
+        expect(options[1]).toHaveAttribute('value', 'test 2');
+        expect(options[2]).toHaveAttribute('value', 'test 3');
+    });
+});
